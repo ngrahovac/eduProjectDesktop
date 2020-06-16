c242514f8dadea8b6383f721186e34c0b8fe16ed
Nina <ninagrahovac.96@gmail.com>
2020-06-16 14:19:28 +0200

    Initial commit

---
+++ src/eduProjectDesktop/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// This code is almost identical to the Blank Application code.
+// The key difference is that this project includes the declaration of 
+// SupportsMultipleInstances in the package.appxmanifest.
+
+namespace eduProjectDesktop
+{
+    sealed partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Suspending += OnSuspending;
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+            if (rootFrame == null)
+            {
+                rootFrame = new Frame();
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                   //TODO: Load state from previously suspended application.
+                }
+                Window.Current.Content = rootFrame;
+            }
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                }
+                Window.Current.Activate();
+            }
+        }
+
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            SuspendingDeferral deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity.
+            deferral.Complete();
+        }
+    }
+}
---
+++ src/eduProjectDesktop/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace eduProjectDesktop
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
